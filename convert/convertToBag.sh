15522f89ca0b6d0d8625a0d31c18cc1bf141d285
Devilox <devilox96@yandex.ru>
2021-12-01 15:02:56 +0300

    Refactored a bit, added ROS version check

--- convert/convertToBag.sh
+++ convert/convertToBag.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(dirname $(readlink -f $0))
+if [[ $(rosversion -d) != "noetic" ]]
+then
+    echo "Your ROS version is not supported!"
+    exit 1
+fi
+
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 ALL='false'
 
 while getopts a: FLAG
 do
     case "${FLAG}" in
         a) ALL='true'
+           break
+           ;;
+        *) echo "Wrong flag!"
     esac
 done
 
@@ -14,24 +23,25 @@ if [[ $ALL == 'true' ]]
 then
     PERSON_DIR=$2
     PERSON_DIR=$(echo "$PERSON_DIR" | sed 's:/*$::')
-    PERSON_DIR=$PERSON_DIR/*
+    PEOPLE_ARR=("$PERSON_DIR"/*)
 else
     PERSON_DIR=$1
     PERSON_DIR=$(echo "$PERSON_DIR" | sed 's:/*$::')
+    PEOPLE_ARR=("$PERSON_DIR")
 fi
 
-for PERSON in $PERSON_DIR
+for PERSON in "${PEOPLE_ARR[@]}"
 do
     if [[ -d "$PERSON" && ! -L "$PERSON" ]]
     then
-        POSES=$PERSON/*
+        POSES=("$PERSON"/*)
 
-        for POSE in $POSES
+        for POSE in "${POSES[@]}"
         do
             if [[ -d "$POSE" && ! -L "$POSE" ]]
             then
                 DIR="$POSE/smartphone_video_frames"
-	            python3 "$SCRIPT_DIR/asl_to_ros.py" --input_path "$POSE/smartphone_video_frames" --output_path "$POSE/smartphone_video_frames/data.bag"
+                python3 "$SCRIPT_DIR/asl_to_ros.py" --input_path "$DIR" --output_path "$DIR/data.bag"
             fi
         done
     fi
